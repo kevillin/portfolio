1a6f7c33f5ef6d5288f72c346c6ef7b6f262f7db
Kevillin Santos <kevillin_santos@hotmail.com>
2023-06-14 10:19:00 -0300

    feat: elimina propriedade border default e limpa estilização original

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import './App.css'
 
 function App() {
   return (
