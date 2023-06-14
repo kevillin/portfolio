8e3d1231f169fe01413b7666437b096972f15a6b
Kevillin Santos <kevillin_santos@hotmail.com>
2023-06-14 10:19:27 -0300

    fix: ajusta imagem para não estourar div

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,7 +9,7 @@ function Main() {
       <h3>Sou Desenvolvedor Web Fullstack Jr.</h3>
       <p>Sou apaixonado por resolver problemas e aprender novas metodologias para serem usadas no mercado de tecnologia. Tenho estudado principalmente a área de desenvolvimento Front-End e UX/UI, utilizando React, Vanilla Javascript, CSS, Figma e testes utilizando Jest. Além disso, me aventuro no Back-End fazendo requisições HTTP utilizando o ThunderClient e consumindo APIs com funções de Fetch utilizando o node.JS, fazendo inserção, remoção e alteração de informações em banco de dados SQL e noSQL.</p>
       </div>
-      <div>
+      <div id="selfie-container">
         <img id="selfie" src={selfieImg} alt="selfie-img" />
       </div>
     </main>
