4c680d8b195a2dd5adc6df0ca9a3172e8b821158
Kevillin Santos <kevillin_santos@hotmail.com>
2023-06-15 19:35:33 -0300

    chore: muda caminho do arquivo de skills

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import NavBar from './components/NavBar'
 import Main from './components/Main'
-import Skills from './components/Skills'
+import Skills from './components/Stacks'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
