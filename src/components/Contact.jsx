b9d530a2da96f5ece0e0b2181379ed54b4646dfc
Kevillin Santos <kevillin_santos@hotmail.com>
2023-07-05 11:21:09 -0300

    melhora componente contact e estiliza

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,26 +7,69 @@ function Contact() {
   return (
     <div>
       <h1 id="contato">Contato</h1>
+      <hr />
       <div className="contact-container">
-        <img className="contact-icon" src={telefoneIcon} alt="telefone-icon" />
-        <h3>+55 (61) 98278-8831</h3>
-        <div className="linha-vertical"></div>
-        <img className="contact-icon" src={localizacaoIcon} alt="localizacao-icon" />
-        <h3>Brasília, DF - Brasil</h3>
-        <div className="linha-vertical"></div>
-        <img className="contact-icon" src={envelopeIcon} alt="envelope-icon" />
-        <h3>kevillin_santos@hotmail.com</h3>
-        <h3>ksantos621@gmail.com</h3>
+        <div className="info-container">
+          <img
+            className="contact-icon"
+            src={telefoneIcon}
+            alt="telefone-icon"
+          />
+          <h3 className="info">+55 (61) 98278-8831</h3>
+        </div>
+        <hr className="corte"/>
+        <div className="info-container">
+          <img
+            className="contact-icon"
+            src={localizacaoIcon}
+            alt="localizacao-icon"
+          />
+          <h3 className="info">Brasília, DF - Brasil</h3>
+        </div>
+        <hr className="corte"/>
+        <div className="info-container">
+          <img
+            className="contact-icon"
+            src={envelopeIcon}
+            alt="envelope-icon"
+          />
+          <h3 className="info">kevillin_santos@hotmail.com</h3>
+          <h3 className="info">ksantos621@gmail.com</h3>
+        </div>
       </div>
       <form>
-        <input type="text" name="name-input" className="input-contact" placeholder="Digite seu nome"/>
-        <input type="email" name="email-input" className="input-contact" placeholder="Digite seu e-mail"/>
-        <input type="text" name="assunto-input" className="input-contact" placeholder="Digite o assunto"/>
-        <textarea name="textarea-input" className="input-textarea" id="" cols="30" rows="10" placeholder="Digite sua mensagem"></textarea>
-        <button type="submit" className="button-contact">Enviar mensagem!</button>
+        <input
+          type="text"
+          name="name-input"
+          className="form-control"
+          placeholder="Digite seu nome"
+        />
+        <input
+          type="email"
+          name="email-input"
+          className="form-control"
+          placeholder="Digite seu e-mail"
+        />
+        <input
+          type="text"
+          name="assunto-input"
+          className="form-control"
+          placeholder="Digite o assunto"
+        />
+        <textarea
+          name="textarea-input"
+          className="form-control"
+          id=""
+          cols="30"
+          rows="10"
+          placeholder="Digite sua mensagem"
+        ></textarea>
+        <button type="submit" className="btn btn-outline-dark">
+          Enviar mensagem!
+        </button>
       </form>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
